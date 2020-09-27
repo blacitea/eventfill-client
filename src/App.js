9e0d4d67efd187fc41b8f2fba35a44965916cdb0
Angel <blacitea@gmail.com>
2020-09-26 23:37:44 -0700

    change App.js from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+const App = props => {
+	const [message, setMessage] = useState('Click the button to load data!');
+	const fetchData = () => {
+		axios
+			.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+			.then(response => {
+				// handle success
+				console.log(response.data); // The entire response from the Rails API
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+				console.log(response.data.message); // Just the message
+				setMessage(response.data.message);
+			});
+	};
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
+	return (
+		<div className="App">
+			<h1>{message}</h1>
+			<button onClick={fetchData}>Fetch Data</button>
+		</div>
+	);
+};
 
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       message: 'Click the button to load data!'
+//     }
+//   }
+
+//   fetchData = () => {
+//     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+//     .then((response) => {
+//       // handle success
+//       console.log(response.data) // The entire response from the Rails API
+
+//       console.log(response.data.message) // Just the message
+//       this.setState({
+//         message: response.data.message
+//       });
+//     })
+//   }
+
+//   render() {
+//     return (
+
+//     );
+//   }
+// }
 
 export default App;
