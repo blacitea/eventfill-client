c1b5e763648f01cc7a31d09fadb0d0c4b52011a9
Angel <blacitea@gmail.com>
2020-09-28 10:37:56 -0700

    add link for aspect control & comment for renderEventContent

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,11 +4,12 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import './Calendar.scss';
 
 const Calendar = props => {
+	//Control what data to display in grid
 	function renderEventContent(eventInfo) {
 		return (
-			<article>
+			<article className="calendar-grid">
 				<p>{eventInfo.event.title}</p>
-				<p>{eventInfo.event.extendedProps.description}</p>
+				{/* <p>{eventInfo.event.extendedProps.description}</p> */}
 				<img
 					className="calendar-grid-thumbnail"
 					src={eventInfo.event.extendedProps.thumbnail}
@@ -18,13 +19,19 @@ const Calendar = props => {
 		);
 	}
 	return (
-		<FullCalendar
-			plugins={[dayGridPlugin]}
-			initialView="dayGridMonth"
-			initialEvents={props.events}
-			eventClick={props.onClick}
-			eventContent={renderEventContent}
-		/>
+		<section className="calendar">
+			<FullCalendar
+				height="auto"
+				// contentHeight="100"
+				// width to height ration (https://fullcalendar.io/docs/aspectRatio)
+				aspectRatio="1"
+				plugins={[dayGridPlugin]}
+				initialView="dayGridMonth"
+				initialEvents={props.events}
+				eventClick={props.onClick}
+				eventContent={renderEventContent}
+			/>
+		</section>
 	);
 };
 
