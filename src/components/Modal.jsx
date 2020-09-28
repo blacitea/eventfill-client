d422cc6c8d5bf6f1df8ba2130b73d5a194b2e73b
Quinn <quinnvoker@gmail.com>
2020-09-28 11:04:24 -0700

    modal now properly displays children

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,14 +3,16 @@ import ReactModal from 'react-modal';
 
 import './Modal.scss'
 
-const Modal = ({isOpen}) => {
+const Modal = ({isOpen, ...props}) => {
   return (
     <ReactModal
       isOpen={isOpen}
       closeTimeoutMS={500}
       className='modal-container'
       overlayClassName='modal-overlay'
-    />
+    >
+      {props.children}
+    </ReactModal>
   )
 }
 
