a6354080684a4de50d4364cca2a6587de723c422
Angel <blacitea@gmail.com>
2020-09-29 16:11:14 -0700

    add links for all li

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,9 @@ import { Link } from 'react-router-dom';
 const NavBar = props => {
 	return (
 		<nav>
-			<section className="logo">EVENTFILL</section>
+			<Link className="router-link" to="/">
+				<section className="logo">EVENTFILL</section>
+			</Link>
 			<ul className="links">
 				<Link to="/">
 					<li>Home</li>
@@ -13,9 +15,15 @@ const NavBar = props => {
 				<Link to="/events">
 					<li>Events</li>
 				</Link>
-				<li>Talents</li>
-				<li>My Profile</li>
-				<li>Log Out</li>
+				<Link to="/talents">
+					<li>Talents</li>
+				</Link>
+				<Link to="/profile">
+					<li>My Profile</li>
+				</Link>
+				<Link to="/">
+					<li>Log Out</li>
+				</Link>
 			</ul>
 		</nav>
 	);
