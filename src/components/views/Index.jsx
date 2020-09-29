4275f15a148a9eca5701bb2ff9527130a533a0e7
Angel <blacitea@gmail.com>
2020-09-29 14:29:26 -0700

    update with all components for index and props pass on

--- src/components/views/Index.jsx
+++ src/components/views/Index.jsx
@@ -5,7 +5,25 @@ import HighligthsList from '../HighlightsList';
 const Index = props => {
 	return (
 		<main>
-			<section className="index-focus"></section>
+			<section className="index-focus">
+				<article>
+					<h1>Your Event Starts Here</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+						faucibus mi et lobortis euismod. Vivamus pellentesque gravida urna,
+						non semper nisi pellentesque in. Nam quis dictum ex. Proin placerat,
+						sapien at consequat scelerisque, nulla turpis sagittis leo, ac
+						iaculis elit urna vitae lorem.
+					</p>
+					<button>Start an event!</button>
+				</article>
+				<aside>
+					<img
+						src="http://www.manaleak.com/mtguk/files/2016/11/Stoneforge-Mystic-1.jpg"
+						alt="main poster"
+					/>
+				</aside>
+			</section>
 			<PreviewsList
 				title="Upcoming Events Near You"
 				message="Check out these events and plan your attendance!"
@@ -15,7 +33,10 @@ const Index = props => {
 			<HighligthsList
 				title="Our hottest Talent profiles!"
 				array={props.talents}
+				onClick={props.onClick}
 			/>
 		</main>
 	);
 };
+
+export default Index;
