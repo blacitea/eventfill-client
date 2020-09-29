7298b5d797a9389180943fb94a71b75dd14db5a3
Quinn <quinnvoker@gmail.com>
2020-09-29 09:04:46 -0700

    fixed hacky spacing solution

--- src/components/Highlight.jsx
+++ src/components/Highlight.jsx
@@ -4,16 +4,18 @@ import './Highlight.scss'
 
 const Highlight = ({onClick, imageURL, name}) => {
   return (
-    <figure className="highlight" onClick={onClick}>
-      <img
-        className="highlight-image"
-        src={imageURL}
-        alt={name}
-      />
-      <figcaption className="highlight-caption">
-        {name}
-      </figcaption>
-    </figure>
+    <section className="highlight-container">
+      <figure className="highlight" onClick={onClick}>
+        <img
+          className="highlight-image"
+          src={imageURL}
+          alt={name}
+        />
+        <figcaption className="highlight-caption">
+          {name}
+        </figcaption>
+      </figure>
+    </section>
   )
 }
 
