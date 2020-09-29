465dcdc36a5a07e1583a9cd236303e3bf72fda2b
Angel <blacitea@gmail.com>
2020-09-29 15:55:52 -0700

    use Link to pair up with React Router

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './NavBar.scss';
+import { Link } from 'react-router-dom';
 
 const NavBar = props => {
-	const [user, setUser] = useState(false);
-	console.log(user);
 	return (
 		<nav>
 			<section className="logo">EVENTFILL</section>
 			<ul className="links">
-				<li>Events</li>
+				<Link to="/">
+					<li>Home</li>
+				</Link>
+				<Link to="/events">
+					<li>Events</li>
+				</Link>
 				<li>Talents</li>
 				<li>My Profile</li>
-        <li>Log Out</li>
+				<li>Log Out</li>
 			</ul>
 		</nav>
 	);
