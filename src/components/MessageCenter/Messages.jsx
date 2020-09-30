ca55816a91d0a624604b89d2081dc84c28317324
Angel <blacitea@gmail.com>
2020-09-30 13:04:15 -0700

    build a skeleton for message board

---
+++ src/components/MessageCenter/Messages.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Message from './Message';
+
+const Messages = ({ messages, recipient }) => {
+	return (
+		<section className="messages-board">
+			<p className="messages-board-recipient-name">
+				{recipient && recipient.name}
+			</p>
+			{messages &&
+				messages.map(message => (
+					<Message
+						content={message.content}
+						sender={message.sender_id}
+						timeStamp={message.sent_at}
+					/>
+				))}
+		</section>
+	);
+};
+
+export default Messages;
