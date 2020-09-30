5d387b9d88ececbf13affdbe52ca5014bccfe8c2
Angel <blacitea@gmail.com>
2020-09-29 23:43:06 -0700

    add create button with Link

--- src/components/views/Explore.jsx
+++ src/components/views/Explore.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import PreviewsList from '../PreviewsList';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 const ExploreEvents = props => {
 	let { id } = useParams();
 	return (
 		<main>
 			<header>
-				<h1 className="title">Explore Events</h1>
+				<h1 className="title">
+					{id === 'events' ? 'Explore Events' : 'Explore Talents'}
+				</h1>
 				<section className="nav">
-					<button>Location</button>
-					<button>Type</button>
+					<button onClick={() => alert("filter logic by user's location")}>
+						Location
+					</button>
+					<button onClick={() => alert('filter logic by genre')}>Genre</button>
+					<Link to={`/create/${id === 'events' ? 'event' : 'talent'}`}>
+						<button>Create</button>
+					</Link>
 				</section>
 			</header>
 			<hr />
