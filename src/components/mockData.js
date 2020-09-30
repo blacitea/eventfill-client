ab3352efd0ed9f9f2b40e047f479322f04f14cc7
Angel <blacitea@gmail.com>
2020-09-29 19:18:40 -0700

    fix missing desc for talent profiles

--- src/components/mockData.js
+++ src/components/mockData.js
@@ -4,7 +4,8 @@ export const events = [
 		user_id: 1,
 		name: 'Event 1',
 		start: '2020-09-22',
-		description: 'Our event is going to be the biggest ever so come and see it if you want to see the biggest event',
+		description:
+			'Our event is going to be the biggest ever so come and see it if you want to see the biggest event',
 		image_url:
 			'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQNMSYTAILIUcNLVGZ7JNz4xt96spL4zpv3nQ&usqp=CAU',
 		url: '#',
@@ -48,6 +49,8 @@ export const talents = [
 		name: 'Nicol Bolas',
 		image_url:
 			'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQNMSYTAILIUcNLVGZ7JNz4xt96spL4zpv3nQ&usqp=CAU',
+		description:
+			'Aliquam orci tellus, venenatis vel suscipit id, aliquet sed arcu. Curabitur interdum enim a orci luctus, non fermentum ligula tincidunt. Donec at orci a ipsum ullamcorper dignissim eget vel lectus. Aliquam erat volutpat. Praesent sollicitudin convallis faucibus. Curabitur ac suscipit magna, non rhoncus mauris. Cras ac lorem magna. Fusce vitae tellus nisi. Integer tristique iaculis est, in euismod nisl condimentum eu.',
 	},
 	{
 		id: 2,
@@ -55,24 +58,32 @@ export const talents = [
 		name: 'Nahiri',
 		image_url:
 			'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgy3Wp1EbrYefjQnpJ3w9RWkVe3P-bQsepDA&usqp=CAU',
+		description:
+			'Quisque purus lorem, ultrices vel ipsum ac, commodo pretium nisl. Nulla at viverra purus. Vivamus turpis eros, dignissim vel finibus at, efficitur at dui. Donec rhoncus condimentum quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus fermentum nunc quis diam viverra, finibus consequat dui viverra. Donec lorem risus, mollis sollicitudin dui sit amet, finibus tristique orci. Fusce velit nulla, dapibus eu ultricies sed, aliquet ac ex.',
 	},
 	{
 		id: 3,
 		user_id: 4,
 		name: 'Liliana',
 		image_url: 'https://i.imgur.com/TRrV9e6.jpg',
+		description:
+			'Ut turpis risus, fermentum at est eget, feugiat pretium odio. Duis placerat, mi eu pretium imperdiet, diam nunc iaculis diam, lobortis dictum nunc risus in erat. Duis nibh urna, cursus vitae enim sed, placerat accumsan urna. Praesent rutrum facilisis sapien a pharetra. Morbi elementum mauris et massa sollicitudin, ut suscipit tellus tincidunt. Vestibulum blandit maximus risus. Donec elementum elementum nulla sit amet sollicitudin. Nulla non mi a mauris hendrerit ultricies. ',
 	},
 	{
 		id: 4,
 		user_id: 1,
 		name: 'Nissa',
 		image_url: 'https://i.imgur.com/59LE47m.jpg',
+		description:
+			'Nam ligula mi, fermentum eu rutrum ac, facilisis non sapien. Nulla elementum odio lacus, quis aliquam velit iaculis eu. Quisque eget tempor tortor, eget elementum tellus. Aliquam vel laoreet urna, vitae venenatis magna. Curabitur lectus tellus, rutrum id sapien ac, posuere semper massa. Nam bibendum ligula est. Praesent sit amet nulla nunc. Curabitur elit risus, molestie in tristique non, lobortis vitae ante. Donec rhoncus, lectus vitae tincidunt dignissim, ex orci porttitor arcu, sed lobortis turpis magna nec purus. Donec mi tortor, suscipit at elit in, rutrum auctor turpis.',
 	},
 	{
 		id: 5,
 		user_id: 2,
 		name: 'Jace',
 		image_url: 'https://i.imgur.com/kCv2rRe.jpg',
+		description:
+			'Praesent posuere ligula sit amet tellus porttitor, id lobortis lacus accumsan. Proin sit amet nisi felis. Aenean mattis rhoncus lectus, eget cursus quam rhoncus nec. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec egestas odio dignissim velit tincidunt, id fringilla libero volutpat. Proin convallis, nunc ut porta placerat, mi nisl faucibus orci, hendrerit facilisis odio sapien et mauris.',
 	},
 ];
 
