217a47b226d02d1e58a6eaba1fcef4b0c00fb01d
Angel <blacitea@gmail.com>
2020-10-01 15:14:50 -0700

    add toggle func to close message on 2nd click

--- src/components/MessageCenter/Contacts.jsx
+++ src/components/MessageCenter/Contacts.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Contact from './Contact';
 
-const Contacts = ({ contactList, setRecipient }) => {
+const Contacts = ({ contactList, setRecipient, recipient }) => {
+	const toggleRecipient = id => {
+		id === recipient ? setRecipient('') : setRecipient(id);
+	};
 	return (
 		<section className="message-center-contacts">
 			{contactList.map(contact => {
 				return (
 					<Contact
 						key={contact.id}
-						onClick={() => setRecipient(contact.id)}
+						onClick={() => toggleRecipient(contact.id)}
 						name={contact.name}
 					/>
 				);
