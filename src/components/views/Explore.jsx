71e1bbc2080a4552d6648c4c1a761e01ef4ed96b
Quinn <quinnvoker@gmail.com>
2020-10-01 09:23:57 -0700

    enabled selection of "all"

--- src/components/views/Explore.jsx
+++ src/components/views/Explore.jsx
@@ -14,8 +14,9 @@ const Explore = props => {
 
   useEffect(() => {
     // axios call to get relevant filtered list of events/profiles here
+    // set the results of call to collection to have it displayed in previewlist
 
-    // simulating results of api call
+    // simulating results of api call below
     console.log(`location: ${location || null}, genre ${genre || null}`);
 
     let newCollection = id === "events" ? events : talents;
@@ -40,8 +41,8 @@ const Explore = props => {
             defaultValue="" 
             onChange={(event) => setLocation(parseInt(event.target.value))}
           >
-            <option value="" disabled >
-              Location
+            <option value="" >
+              All Locations
             </option>
             {locations &&
               locations.map(location => (
@@ -55,8 +56,8 @@ const Explore = props => {
             defaultValue="" 
             onChange={(event) => setGenre(parseInt(event.target.value))}
           >
-            <option value="" disabled >
-              Genre
+            <option value="" >
+              All Genres
             </option>
             {genres &&
               genres.map(genre => (
