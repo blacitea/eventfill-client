9443c995e0143cdb7937b5342e1777de759c964b
Angel <blacitea@gmail.com>
2020-09-30 17:24:29 -0700

    remove cookies from NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 const NavBar = props => {
 	const [open, setOpen] = useState(false);
+	console.log(props.cookies.user_id);
 
 	const Dropdown = props => {
 		return (
@@ -40,7 +41,7 @@ const NavBar = props => {
 					Create
 				</li>
 				{open && <Dropdown />}
-				<Link to={`/messages/1`}>
+				<Link to="/messages">
 					<li>My Messages</li>
 				</Link>
 			</ul>
