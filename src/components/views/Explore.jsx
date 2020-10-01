0b6713d9e239e30d249a98d1286b5a71ca06c0f4
Quinn <quinnvoker@gmail.com>
2020-10-01 08:52:02 -0700

    set up selectors for genre and location

--- src/components/views/Explore.jsx
+++ src/components/views/Explore.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PreviewsList from '../PreviewsList';
 import { useParams, Link } from 'react-router-dom';
 import './Explore.scss';
 
 const Explore = props => {
-	let { id } = useParams();
+  let { id } = useParams();
+  const [location, setLocation] = useState('');
+  const [genre, setGenre] = useState('');
+
+  useEffect(() => {
+    console.log(`location: ${location || null}, genre ${genre || null}`);
+    // axios call to get relevant filtered list of events/profiles here
+  }, [location, genre])
+
+
 	return (
 		<main className="explore">
 			<header>
@@ -12,10 +21,30 @@ const Explore = props => {
 					{id === 'events' ? 'Explore Events' : 'Explore Talents'}
 				</h1>
 				<section className="nav">
-					<button onClick={() => alert("filter logic by user's location")}>
-						Location
-					</button>
-					<button onClick={() => alert('filter logic by genre')}>Genre</button>
+          <select 
+            className='location-select' 
+            defaultValue="" 
+            onChange={(event) => setLocation(event.target.value)}
+          >
+            <option value="" disabled >
+              Location
+            </option>
+            <option value="location-test">
+              TEST
+            </option>
+          </select>
+          <select 
+            className='location-select' 
+            defaultValue="" 
+            onChange={(event) => setGenre(event.target.value)}
+          >
+            <option value="" disabled >
+              Genre
+            </option>
+            <option value="genre-test">
+              TEST
+            </option>
+          </select>
 					<Link
 						to={`/create/${id === 'events' ? 'event' : 'talent'}`}
 						className="create-button"
