b3c284fd1f02e079eec126263dd0398ef1f61343
Quinn <quinnvoker@gmail.com>
2020-10-02 10:59:55 -0700

    added links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,11 +13,21 @@ const Footer = props => {
 			</ul>
 			<hr></hr>
 			<ul className="footer-thumbnails">
-				<img src="/temp1.png" alt="Events" />
-				<img src="/temp2.png" alt="Talents" />
-				<img src="/temp3.png" alt="EVENTFILL" />
-				<img src="/temp4.png" alt="About" />
-				<img src="/temp5.png" alt="Blog" />
+        <a href="https://www.lighthouselabs.ca/">
+				  <img src="http://github.com/lighthouse-labs.png" alt="Lighthouse Labs" />
+        </a>
+        <a href="http://github.com/blacitea">
+			  	<img src="http://github.com/blacitea.png" alt="Angel Tsang" />
+        </a>
+        <a href="http://github.com/risatronic">
+			  	<img src="http://github.com/risatronic.png" alt="Marisa Doig" />
+        </a>
+        <a href="http://github.com/quinnvoker">
+	  			<img src="http://github.com/quinnvoker.png" alt="Quinn Branscombe" />
+        </a>
+        <a href="http://github.com/eventfillers">
+				  <img src="http://github.com/eventfillers.png" alt="Eventfillers" />
+        </a>
 			</ul>
 		</section>
 	);
