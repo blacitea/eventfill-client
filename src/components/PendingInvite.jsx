1818bc83998dfecb428d0c4920faa8aff154609f
Angel <blacitea@gmail.com>
2020-10-02 20:24:02 -0700

    invitation response

---
+++ src/components/PendingInvite.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+import axios from 'axios';
+import './PendingInvite.scss';
+
+const PendingInvite = ({ pendingGig }) => {
+	console.log(pendingGig);
+	const [rvsp, setRVSP] = useState(pendingGig.accepted);
+	const handleClick = response => {
+		axios
+			.patch(`/api/gigs/${pendingGig.id}`, {
+				gig: { ...pendingGig, accepted: response },
+			})
+			.then(resolve => setRVSP(response))
+			.catch(error => console.log('Error!', error));
+	};
+
+	return (
+		<article className="event-invite">
+			<p className="event-invite-tag">
+				You have an invitation to perform at this event!
+			</p>
+			{rvsp === null && (
+				<section className="event-invite-options">
+					<button
+						className="event-invite-btn-accept"
+						onClick={() => handleClick(true)}
+					>
+						Accept
+					</button>
+					<button
+						className="event-invite-btn-decline"
+						onClick={() => handleClick(false)}
+					>
+						Decline
+					</button>
+				</section>
+			)}
+			<article className="event-invite-response">
+				{rvsp === true && 'Invitation was accepted!'}
+				{rvsp === false && 'Invitation was declined!'}
+			</article>
+		</article>
+	);
+};
+
+export default PendingInvite;
