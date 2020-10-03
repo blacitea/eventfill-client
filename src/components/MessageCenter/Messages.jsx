81eb07d8e0bda9375ef68af935541cc658bc90b1
Quinn <quinnvoker@gmail.com>
2020-10-03 10:23:04 -0700

    scroll now only on messages

--- src/components/MessageCenter/Messages.jsx
+++ src/components/MessageCenter/Messages.jsx
@@ -5,22 +5,24 @@ const Messages = ({ messages, recipient, owner }) => {
   console.log(messages)
 
 	return (
-		<section className="messages-board">
-			<p className="messages-board-recipient-name">
-				{recipient.name && recipient.name}
-			</p>
-			{recipient.name &&
-				messages &&
-				messages.map(message => (
-					<Message
-						owner={owner}
-						key={message.id}
-						content={message.content}
-						sender={message.sender_id}
-						timeStamp={message.created_at}
-					/>
-				))}
-		</section>
+    <section>
+      <p className="messages-recipient-name">
+        {recipient.name && recipient.name}
+      </p>
+      <section className="messages-board">
+        {recipient.name &&
+          messages &&
+          messages.map(message => (
+            <Message
+              owner={owner}
+              key={message.id}
+              content={message.content}
+              sender={message.sender_id}
+              timeStamp={message.created_at}
+            />
+          ))}
+      </section>
+    </section>
 	);
 };
 
