8b6b52dd97a70391edb005e152de3129a3010a5a
Angel <blacitea@gmail.com>
2020-10-03 13:45:45 -0700

    update message to display a link to event if message is a auto gen from invitation

--- src/components/MessageCenter/Message.jsx
+++ src/components/MessageCenter/Message.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 const Message = ({ content, timeStamp, sender, owner }) => {
 	const cs =
 		sender === owner ? /*cookie.user_id*/ 'message-me' : 'message-them';
+	const eventId = content.includes('Score')
+		? content.slice(content.indexOf('#') + 1)
+		: null;
+	console.log(eventId);
+	const link = (
+		<Link to={`/events/${eventId}`}>
+			<p>Go check out the event!</p>
+		</Link>
+	);
 	return (
 		<article className={cs}>
 			<p>{content}</p>
+			{eventId && link}
 			<footer>{timeStamp}</footer>
 		</article>
 	);
