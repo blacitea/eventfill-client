6056c73b4c5a9215aa2a3649d6273092548dcb82
Quinn <quinnvoker@gmail.com>
2020-10-02 19:28:39 -0700

    basic message styling

--- src/components/MessageCenter/Message.jsx
+++ src/components/MessageCenter/Message.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import moment from 'moment';
+
 const Message = ({ content, timeStamp, sender, owner }) => {
 	const cs =
 		sender === owner ? /*cookie.user_id*/ 'message-me' : 'message-them';
 	return (
-		<article className={cs}>
-			<p>{content}</p>
-			<footer>{timeStamp}</footer>
+		<article className={"message " + cs}>
+			<p className="message-content">{content}</p>
+      <footer className="message-time">{sender === owner ? "You, " : "Them, "}{moment(timeStamp).fromNow()}</footer>
 		</article>
 	);
 };
