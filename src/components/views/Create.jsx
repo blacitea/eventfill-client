86c498a8626d42a7ffa8a4d59dce5f76869a77fa
Angel <blacitea@gmail.com>
2020-10-04 22:59:07 -0700

    add message to require user login for create event/talent profile

--- src/components/views/Create.jsx
+++ src/components/views/Create.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import EventForm from '../forms/EventForm';
 import TalentForm from '../forms/TalentForm';
+import { useCookies } from 'react-cookie';
 
 import './Create.scss';
 
@@ -22,9 +23,11 @@ const TALENT_ASIDE = (
 	</>
 );
 
-
 const Create = ({ locations, genres, openModal }) => {
 	let { id } = useParams();
+	const [cookies] = useCookies();
+	const user = parseInt(cookies.user_id);
+
 	useEffect(() => {
 		document.title = 'EVENTFILL - Creation';
 		return () => {
@@ -38,9 +41,27 @@ const Create = ({ locations, genres, openModal }) => {
 				{id === 'talent' && TALENT_ASIDE}
 			</aside>
 			<section className="create-form">
-				{id === 'event' && <EventForm locations={locations} genres={genres} openModal={openModal} />}
-				{id === 'talent' && (
-					<TalentForm locations={locations} genres={genres} openModal={openModal} />
+				{isNaN(user) && (
+					<article className="err">
+						<p className="err-login">
+							Please login to create{' '}
+							{id === 'event' ? 'an event' : 'a talent profile'}
+						</p>
+					</article>
+				)}
+				{!isNaN(user) && id === 'event' && (
+					<EventForm
+						locations={locations}
+						genres={genres}
+						openModal={openModal}
+					/>
+				)}
+				{!isNaN(user) && id === 'talent' && (
+					<TalentForm
+						locations={locations}
+						genres={genres}
+						openModal={openModal}
+					/>
 				)}
 			</section>
 		</main>
