9f8c1f615eb8f5ceb1571014e90a7d79343a0921
Angel <blacitea@gmail.com>
2020-10-04 23:34:19 -0700

    redo message center for system message

--- src/components/MessageCenter/MessageList.jsx
+++ src/components/MessageCenter/MessageList.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import Message from './Message';
 
-import './MessageList.scss'
+import './MessageList.scss';
 
 const Messages = ({ messages, recipient, owner }) => {
+	console.log(recipient);
 	return (
-    <section>
-      <p className="recipient-name">
-        {recipient.name && recipient.name}
-      </p>
-      <section id="message-list" className="message-list">
-        {recipient.name &&
-          messages &&
-          messages.map(message => (
-            <Message
-              owner={owner}
-              key={message.id}
-              content={message.content}
-              sender={message.sender_id}
-              timeStamp={message.created_at}
-            />
-          ))}
-      </section>
-    </section>
+		<section>
+			<p className="recipient-name">{recipient.name}</p>
+			<section id="message-list" className="message-list">
+				{recipient.name &&
+					messages &&
+					messages.map(message => (
+						<Message
+							owner={owner}
+							key={message.id}
+							content={message.content}
+							sender={message.sender_id}
+							timeStamp={message.created_at}
+						/>
+					))}
+			</section>
+		</section>
 	);
 };
 
