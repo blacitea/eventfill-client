6b03127b60397f3ac99289548ace8a0eff9d0fac
Quinn <quinnvoker@gmail.com>
2020-10-06 19:31:32 -0700

    replaced footer images with local copies

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,19 +25,19 @@ const Footer = props => {
 			<hr></hr>
 			<ul className="footer-thumbnails">
         <a href="https://www.lighthouselabs.ca/">
-				  <img src="http://github.com/lighthouse-labs.png" alt="Lighthouse Labs" />
+				  <img src="/images/avatars/lhl.png" alt="Lighthouse Labs" />
         </a>
         <a href="http://github.com/blacitea">
-			  	<img src="http://github.com/blacitea.png" alt="Angel Tsang" />
+			  	<img src="/images/avatars/angel.jpeg" alt="Angel Tsang" />
         </a>
         <a href="http://github.com/risatronic">
-			  	<img src="http://github.com/risatronic.png" alt="Marisa Doig" />
+			  	<img src="/images/avatars/marisa.jpeg" alt="Marisa Doig" />
         </a>
         <a href="http://github.com/quinnvoker">
-	  			<img src="http://github.com/quinnvoker.png" alt="Quinn Branscombe" />
+	  			<img src="/images/avatars/quinn.png" alt="Quinn Branscombe" />
         </a>
         <a href="http://github.com/eventfillers">
-				  <img src="http://github.com/eventfillers.png" alt="Eventfillers" />
+				  <img src="/images/avatars/eventfill.png" alt="Eventfillers" />
         </a>
 			</ul>
 		</section>
