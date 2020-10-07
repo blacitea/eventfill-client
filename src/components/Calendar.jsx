448a71f9004fd4c9121d7b1da558658131630d21
Quinn <quinnvoker@gmail.com>
2020-10-07 14:41:16 -0700

    fix calendar display

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -12,19 +12,9 @@ const Calendar = props => {
 		console.log('Did this happen?');
 		axios.get('/api/events').then(resolve => {
 			const eList = resolve.data.map(e => {
-				console.log('before change start', e.start);
-				let startd = moment(e.start).add(1, 'days').toISOString();
-
-				console.log('after changed', startd);
-
-				console.log('before change end', e.end);
-				let endd = moment(e.end).add(1, 'days').toISOString();
-				console.log('after changed', endd);
 				return {
 					...e,
 					url: `/events/${e.id}`,
-					start: startd,
-					end: endd,
 				};
 			});
 
